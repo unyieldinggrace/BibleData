3b42b1c836217d93d39406508af573591d3c0792
Nick Watts <nick@cravingreality.net>
2023-05-27 12:20:26 +1000

    Outlined script for adding niqud column to DB.

---
+++ addNiqud.php
@@ -0,0 +1,9 @@
+<?php
+
+// Open connection to DB file with SQLite
+// Loop over books, chapters, verses, words
+// Run diatheke for target verse
+// Extract word from verse
+// Compare word form without niqud to what is currently in DB. If it's a match, insert niqud form.
+// If not a match, then flag for manual update.
+
